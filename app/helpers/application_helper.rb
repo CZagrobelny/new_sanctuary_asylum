2640b7af85b7cc5aee7086cac02ee1b22b67bbf9
CZagrobelny <zagrobel@gmail.com>
2017-04-12 09:36:49 -0400

    start styling forms with bootstrap and create helper for validation errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def display_validation_errors(resource)
+    if resource.errors.present?
+      content_tag :ul do
+        resource.errors.full_messages.each do |message|
+          concat content_tag(:li, message)
+        end
+      end
+    end
+  end
+
 end
