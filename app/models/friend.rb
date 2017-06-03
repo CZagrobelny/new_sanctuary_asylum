c86f7bf085c99a4ea87ebf7789f98657253ae6d8
CZagrobelny <zagrobel@gmail.com>
2017-06-03 13:51:19 -0400

    add enums

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,7 +1,15 @@
 class Friend < ActiveRecord::Base
+  enum ethnicity: [:white, :black, :hispanic, :asian, :south_asian, :caribbean, :indigenous, :other]
+  enum gender: [:male, :female, :awesome]
+  enum status: [:in_deportation_proceedings, :not_in_deportation_proceedings, :asylum_reciepient, :asylum_application_denied, :legal_permanent_resident, :in_detention, :green_card_holder]
+  enum asylum_status: [:asylum_not_eligible, :asylum_eligible, :asylum_application_started, :asylum_application_completed, :asylum_application_submitted, :asylum_granted, :asylum_denied]
+  enum work_authorization_status: [:work_authorization_not_eligible, :work_authorization_eligible, :work_authorization_application_started, :work_authorization_application_completed, :work_authorization_application_submitted, :work_authorization_granted, :work_authorization_denied]
+  enum sidj_status: [:sidj_not_eligible, :sidj_eligible, :sidj_application_started, :sidj_application_completed, :sidj_application_submitted, :sidj_granted, :sidj_denied]
+
   validates :first_name, :last_name, presence: true
   validates :a_number, presence: { if: :a_number_available? }, numericality: { if: :a_number_available? }
   validates :a_number, length: { minimum: 8, maximum: 9 }, if: :a_number_available?
+
 
   private
 
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -8,15 +8,23 @@ RSpec.describe Friend, type: :model do
   
   describe 'validations' do
 
-  	context '"no_a_number" is true' do
+  	context 'no_a_number is true' do
   		before { friend.no_a_number = true }
   		it { should_not validate_presence_of(:a_number) }
   	end
 
-  	context '"no_a_number" is false' do
+  	context 'no_a_number is false' do
   		it { should validate_presence_of(:a_number) }
   		it { should validate_length_of(:a_number).is_at_most(9).is_at_least(8) }
   		it { should validate_numericality_of(:a_number) }
+  	end
+
+  	context 'date_of_birth is invalid' do
+  		before { friend.date_of_birth = '13/11/2012' }
+  		it 'should display error' do
+  			friend.save
+
+  		end
   	end
 
   end
