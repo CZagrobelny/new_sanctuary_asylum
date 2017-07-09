a95491e39fdceec1498ec2138bcc8b4424c566cc
CZagrobelny <zagrobel@gmail.com>
2017-07-09 16:25:19 -0400

    fixes for chosen rendering

--- app/assets/javascripts/admin_friend_form.js
+++ app/assets/javascripts/admin_friend_form.js
@@ -1,9 +1,5 @@
-$(document).ready(function () {
-  $('.chzn-select').chosen({
-    allow_single_deselect: false,
-    no_results_text: 'No results matched',
-    width: '100%'
-  });
+$(document).on('turbolinks:load', function () {
+  activateChosen();
 
   $('#friend_ethnicity').change(function() {
     if ($(this).find('option:selected').text() == "Other") {
@@ -21,5 +17,14 @@ $(document).ready(function () {
       $('#friend_a_number').val('');
     }
   });
-
 });
+
+function activateChosen() {
+  $('.chzn-select').chosen({
+    allow_single_deselect: false,
+    no_results_text: 'No results matched',
+    width: '100%'
+  });
+}
+
+
