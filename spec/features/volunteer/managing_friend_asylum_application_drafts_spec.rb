c5646bdc210f220c573a61bd289d5c5b01b04f8a
CZagrobelny <zagrobel@gmail.com>
2017-07-09 22:36:01 -0400

    add a test for adding asylum app drafts

--- spec/features/volunteer/managing_friend_asylum_application_drafts_spec.rb
+++ spec/features/volunteer/managing_friend_asylum_application_drafts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Volunteer managing asylum application drafts for shared friends', type: :feature do
+RSpec.describe 'Volunteer managing asylum application drafts for shared friends', type: :feature, js: true do
   let!(:volunteer) { create(:user, :volunteer) }
   let!(:shared_friend) { create(:friend) }
   let!(:not_shared_friend) { create(:friend) }
@@ -19,6 +19,29 @@ RSpec.describe 'Volunteer managing asylum application drafts for shared friends'
 
     it 'does not display friend records NOT associated with the volunteer' do
       expect(page).not_to have_content(not_shared_friend.full_name)
+    end
+  end
+
+  describe 'adding a new asylum application draft' do
+    before do
+      visit friend_path(shared_friend)
+      click_link 'Asylum'
+      click_link 'Add Asylum Application Draft'
+    end
+
+    describe 'with valid information' do
+      it 'displays the new asylum application draft' do
+        volunteer = User.last
+        within '#add_asylum_application_draft_modal' do
+          select_from_chosen(volunteer.name, from: {id: 'asylum_application_draft_user_ids'})
+          fill_in 'Notes', with: 'This is the first draft.'
+          click_button 'Save'
+        end
+
+        within '#asylum-application-draft-list' do
+          expect(page).to have_content('This is the first draft.')
+        end
+      end
     end
   end
 
