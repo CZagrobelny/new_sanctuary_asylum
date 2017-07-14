14cd762dcf5a23cff5a153bf06c7b73c1baf9175
Natasha Jones <tasha.k.jones@gmail.com>
2017-07-14 18:14:26 -0400

    Add textacular

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Search, type: :model do
     let!(:user1) { create :user }  
     let!(:user2) { create :user }
 
-    it "Returns the expected users" do
+    it "returns the expected users" do
       expect(
         Search.new("user", user1.first_name).perform
       ).to eq [user1]
@@ -16,12 +16,17 @@ RSpec.describe Search, type: :model do
     let!(:friend1) { create :friend }
     let!(:friend2) { create :friend }
 
-    it "Returns the expected friends" do
+    it "returns the expected friends if one word is passed" do
       expect(
         Search.new("friend", friend1.last_name).perform
       ).to eq [friend1]
     end
+
+    it "returns the expected friends if two words are passed" do
+      expect(
+        Search.new("friend", friend1.name).perform
+      ).to eq [friend1]
+    end
+
   end
-
-
 end
