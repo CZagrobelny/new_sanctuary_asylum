507306d2c9f6d6f5e2a2337508e4b7ced75bc411
ziggy <ziggy@elephant-bird.net>
2017-07-23 16:29:14 -0400

    delete /admin/judges/:id removes a judge
    and redirects to judges index

--- app/controllers/admin/judges_controller.rb
+++ app/controllers/admin/judges_controller.rb
@@ -1,4 +1,5 @@
 class Admin::JudgesController < AdminController
+  before_action :set_judge, only: [:destroy]
 
   def index
     @judges = Judge.order('created_at desc').paginate(:page => params[:page])
@@ -14,6 +15,19 @@ class Admin::JudgesController < AdminController
   end
 
   def destroy
+    if @judge.destroy
+      flash[:success] = 'Judge successfully removed' 
+    else
+      flash[:error] = 'Something went wrong :( the judge could not be removed' 
+    end
+
+    redirect_to admin_judges_path
+  end
+
+  private
+
+  def set_judge
+    @judge = Judge.find(params[:id])
   end
   
 end
--- spec/controllers/admin/judges_controller_spec.rb
+++ spec/controllers/admin/judges_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin::JudgesController, type: :controller do
   it { should route(:get, '/admin/judges').to(action: :index) }
+  it { should route(:delete, '/admin/judges/123').to(action: :destroy, id: '123') }
 
   describe 'requests' do
     before do
@@ -15,7 +16,6 @@ RSpec.describe Admin::JudgesController, type: :controller do
                            .with('created_at desc')
                            .and_return(double(:paginate => [build(:judge), build(:judge)]))
         get :index
-
       end
     
       it { should respond_with(:success) }
@@ -23,7 +23,38 @@ RSpec.describe Admin::JudgesController, type: :controller do
       it 'assigns @judges' do
         expect(controller.instance_variable_get('@judges').length).to eq 2
       end
+    end
 
+    describe 'DELETE /admin/judges/id' do
+      
+      before do
+        @judge = create(:judge)
+        expect(Judge).to receive(:find).with('123').and_return(@judge)
+        @delete_request = proc { delete :destroy, params: {id: '123'} }
+      end
+
+      it 'removes a judge' do
+        expect(&@delete_request).to change { Judge.count }.by(-1)
+      end
+      
+      it 'redirects to judges index' do
+        @delete_request.call
+        expect(response).to have_http_status 302
+        expect(response.location).to include '/admin/judges'
+      end
+
+      it 'sets success flash' do
+        @delete_request.call
+        expect(flash[:success].present?).to be true
+        expect(flash[:error].present?).to be false
+      end
+
+      it 'sets failure flash' do
+        expect(@judge).to receive(:destroy).and_return(false)
+        @delete_request.call
+        expect(flash[:success].present?).to be false
+        expect(flash[:error].present?).to be true
+      end
     end
   end
 end
