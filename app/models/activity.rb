debc8152e1218d2f9e26aaa5d5b2e9ecaccfb6e1
Natasha Jones <tasha.k.jones@gmail.com>
2017-07-23 20:25:56 -0400

    Add the events table

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -19,14 +19,22 @@ class Activity < ActiveRecord::Base
 
   def self.for_week(beginning_of_week:, end_of_week:, order:, events: Activity.events.keys)
     { dates: "#{beginning_of_week.strftime('%B %-d')} - #{(end_of_week - 2.days).strftime('%B %-d')}", 
+
       activities: Activity.where(event: events)
                           .where('occur_at >= ? AND occur_at <= ? ', beginning_of_week, end_of_week)
                           .order("occur_at #{order}").group_by {|activity| activity.occur_at.to_date } }
   end
 
   def self.upcoming_two_weeks
-    activities = [ Activity.for_week(beginning_of_week: Date.today.beginning_of_week, end_of_week: Date.today.end_of_week, order: 'asc', events: ['check_in', 'master_calendar_hearing', 'individual_hearing']) ]
-    activities << Activity.for_week(beginning_of_week: 1.weeks.from_now.beginning_of_week, end_of_week: 1.weeks.from_now.end_of_week, order: 'asc',  events: ['check_in', 'master_calendar_hearing', 'individual_hearing'])
+    activities = [ Activity.for_week(beginning_of_week: Date.today.beginning_of_week.beginning_of_day, 
+                                     end_of_week: Date.today.end_of_week.end_of_day, 
+                                     order: 'asc', 
+                                     events: ['check_in', 'master_calendar_hearing', 'individual_hearing']) ]
+
+    activities << Activity.for_week(beginning_of_week: 1.weeks.from_now.beginning_of_week, 
+                                    end_of_week: 1.weeks.from_now.end_of_week, 
+                                    order: 'asc',  
+                                    events: ['check_in', 'master_calendar_hearing', 'individual_hearing'])
     activities
   end
 
