42dd778dbaff24da67e61821f07a9f30ed7a549d
CZagrobelny <zagrobel@gmail.com>
2017-07-25 23:05:17 -0400

    carrierwave aws config

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,20 @@
+CarrierWave.configure do |config|
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_acl    = 'public-read'
+
+  # The maximum period for authenticated_urls is only 7 days.
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+  # Set custom options such as cache control to leverage browser caching
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  }
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            ENV.fetch('AWS_REGION') # Required
+  }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723144553) do
+ActiveRecord::Schema.define(version: 20170724025434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20170723144553) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.integer  "location_id"
+    t.string   "title",          default: ""
+    t.string   "event_catagory"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "friend_languages", force: :cascade do |t|
@@ -77,9 +86,9 @@ ActiveRecord::Schema.define(version: 20170723144553) do
     t.date     "date_eligible_to_apply_for_work_authorization"
     t.date     "date_work_authorization_submitted"
     t.text     "work_authorization_notes"
-    t.integer  "sidj_status"
-    t.date     "date_sidj_submitted"
-    t.text     "sidj_notes"
+    t.integer  "sijs_status"
+    t.date     "date_sijs_submitted"
+    t.text     "sijs_notes"
     t.text     "asylum_notes"
     t.integer  "country_id"
     t.integer  "lawyer_referred_to"
