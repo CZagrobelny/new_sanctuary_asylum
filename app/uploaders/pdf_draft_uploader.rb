c8184c9f32988b13cf89e4726a3fa66f1546b80d
CZagrobelny <zagrobel@gmail.com>
2017-07-27 08:46:22 -0400

    only use s3 on prod and styling changes for modal

--- app/uploaders/pdf_draft_uploader.rb
+++ app/uploaders/pdf_draft_uploader.rb
@@ -5,7 +5,11 @@ class PdfDraftUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :aws
+  if Rails.env.production?
+    storage :aws
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
