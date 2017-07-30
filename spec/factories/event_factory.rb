c38ae77171ab2184b9b8281471fe5773a6e1d791
CZagrobelny <zagrobel@gmail.com>
2017-07-30 17:23:14 -0400

    update event factory

--- spec/factories/event_factory.rb
+++ spec/factories/event_factory.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :event do
     association :location
-    title {'Title'}
-    category {'Category'}
+    title {'Title' }
+    category { Event::CATEGORIES.first[0] }
+    date { Time.now }
   end
 end
