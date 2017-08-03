0a87d9780477b9f161a9422a4c4ddfc350ff58a6
CZagrobelny <zagrobel@gmail.com>
2017-08-02 21:44:32 -0400

    fix specs with smarter day selection helper

--- spec/support/select_from_chosen.rb
+++ spec/support/select_from_chosen.rb
@@ -17,7 +17,7 @@ module Select
     field = options[:from]
     select date.strftime('%Y'), :from => "#{field}_1i" #year
     select date.strftime('%B'), :from => "#{field}_2i" #month
-    select date.strftime('%d'), :from => "#{field}_3i" #day 
+    select date.strftime('%-d'), :from => "#{field}_3i" #day 
     select date.strftime('%H'), :from => "#{field}_4i" #hour
     select date.strftime('%M'), :from => "#{field}_5i" #minute
   end
