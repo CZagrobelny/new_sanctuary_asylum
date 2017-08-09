7e9438e100f3e73de115e9fe2f3a392757f83226
CZagrobelny <zagrobel@gmail.com>
2017-08-08 20:53:18 -0400

    send mail from prod heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'frozen-sea-20640.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'new-sanctuary.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'frozen-sea-20640.herokuapp.com'
+  config.mailer_sender = 'new-sanctuary.herokuapp.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
