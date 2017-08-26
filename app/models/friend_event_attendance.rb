308c630ea87a506d7801df13195b1f664a4fdd5f
CZagrobelny <zagrobel@gmail.com>
2017-08-26 18:48:04 -0400

    validate that ids must be greater than 0

--- app/models/friend_event_attendance.rb
+++ app/models/friend_event_attendance.rb
@@ -2,4 +2,5 @@ class FriendEventAttendance < ActiveRecord::Base
   belongs_to :friend
   belongs_to :event
   validates :friend_id, :event_id, presence: true
+  validates :friend_id, numericality: { greater_than: 0 }
 end
--- app/models/user_event_attendance.rb
+++ app/models/user_event_attendance.rb
@@ -2,4 +2,5 @@ class UserEventAttendance < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   validates :user_id, :event_id, presence: true
+  validates :user_id, numericality: { greater_than: 0 }
 end
