faac2f2590279f6e3f1640c663e3cb9404147000
mitchell <mitchell@mitchells-MBP.fios-router.home>
2017-09-17 19:33:06 -0400

    removing duplicated chosen select controls

--- app/assets/javascripts/admin_friend_form.js
+++ app/assets/javascripts/admin_friend_form.js
@@ -28,11 +28,21 @@ $(document).on('turbolinks:load', function () {
 });
 
 function activateChosen() {
-  $('.chzn-select').chosen({
-    allow_single_deselect: true,
-    no_results_text: 'No results matched',
-    width: '100%'
-  });
+  var all_chosen_selects = $('.chzn-select');
+  for (var i=0; i<all_chosen_selects.length;i++) {
+    var chosen_select = all_chosen_selects[i];
+    $(chosen_select).chosen({
+      allow_single_deselect: true,
+      no_results_text: 'No results matched',
+      width: '100%'
+    });
+
+    // remove duplicated chosen-containers
+    if ($(chosen_select).next().next().hasClass('chosen-container')) {
+      $(chosen_select).next().next().remove();
+    }
+  }
+
 }
 
 
