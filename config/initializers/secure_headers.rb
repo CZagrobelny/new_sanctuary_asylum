cfefdcbfb5337f64b1e3ad04090d4578db9cac75
CZagrobelny <zagrobel@gmail.com>
2017-09-26 23:28:46 -0400

    add a custome config

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,1 +1,12 @@
-SecureHeaders::Configuration.default
+SecureHeaders::Configuration.default do |config|  
+  config.csp = {
+    report_only: Rails.env.production?, # default: false
+    preserve_schemes: true, # default: false.
+    default_src: %w(*), # all allowed in the beginning
+    script_src: %w('self'),
+    connect_src: %w('self'),
+    style_src: %w('self' 'unsafe-inline'),
+    report_uri: ["/csp_report?report_only=#{Rails.env.production?}"]
+  }
+end 
+# https://bauland42.com/ruby-on-rails-content-security-policy-csp/
