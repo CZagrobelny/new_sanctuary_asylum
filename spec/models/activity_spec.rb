498910ae7934a31fad9f090b36c173f02b659dc9
CZagrobelny <zagrobel@gmail.com>
2017-10-15 09:24:51 -0400

    remove location validation spec for activity

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Activity, type: :model do
   it { is_expected.to belong_to :location }
 
   it { is_expected.to validate_presence_of :friend_id }
-  it { is_expected.to validate_presence_of :location_id }
   it { is_expected.to validate_presence_of :occur_at }
   it { is_expected.to validate_presence_of :event }
 
