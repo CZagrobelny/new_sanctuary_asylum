5466563422f3d9f844f91592bc7861762fc3d90f
github username <github email address>
2017-12-03 12:06:46 -0500

    fixed add zipcode migration file

--- db/migrate/20171203165301_add_zipcode_to_friends.rb
+++ db/migrate/20171203165301_add_zipcode_to_friends.rb
@@ -1,5 +1,5 @@
 class AddZipcodeToFriends < ActiveRecord::Migration[5.0]
   def change
-    add_column :friends, :string, :zip_code
+    add_column :friends, :zip_code, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008152532) do
+ActiveRecord::Schema.define(version: 20171203165301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20171008152532) do
     t.integer  "lawyer_referred_to"
     t.integer  "lawyer_represented_by"
     t.integer  "sijs_lawyer"
+    t.string   "zip_code"
   end
 
   create_table "judges", force: :cascade do |t|
