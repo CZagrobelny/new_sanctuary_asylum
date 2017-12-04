17e5978c775eef158fbfb04cdeac38a1a5ddb861
CZagrobelny <zagrobel@gmail.com>
2017-12-04 13:29:38 -0500

    work on validations and refactor

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -3,14 +3,20 @@ class Admin::ReportsController < AdminController
   end
 
   def create
-    if report.valid?
-      #Implement something like:
-      # respond_to do |format|
-      #   format.html
-      #   format.csv { send_data report.csv_string, filename: report.filename }
-      # end
+    if report
+      if report.valid?
+        if report.has_data?
+          #Implement something like:
+          # respond_to do |format|
+          #   format.html
+          #   format.csv { send_data report.csv_string, filename: report.filename }
+          # end
+        else
+          #message letting them know that there are no records for the selected date range and render :new
+      else
+        #error message about missing date params and render :new
     else
-      # render an error and :new
+      # add the an error message about selecting type of report error and render :new
     end
   end
 
--- app/models/activity_report.rb
+++ app/models/activity_report.rb
@@ -1,13 +1,19 @@
 class ActivityReport
   include ActiveModel::Model
 
-  attr_accessor :type, :start_date, :end_date
-  validates :type, :start_date, :end_date, presence: true
+  attr_accessor :start_date, :end_date, :start_day, :start_month, :start_year, :end_day, :end_month, :end_year, :activities
+  validates :start_day, :start_month, :start_year, :end_day, :end_month, :end_year, presence: true
+  DATE_ATTRIBUTE_MAPPING = [['start_day', 'start_date(3i)'], ['start_month', 'start_date(2i)'], ['start_year', 'start_date(1i)'], ['end_day', 'end_date(3i)'], ['end_month', 'end_date(2i)'], ['end_year', 'end_date(1i)']] 
 
   def initialize(attributes={})
-    @start_date = Date.new(attributes['start_date(1i)'].to_i, attributes['start_date(2i)'].to_i, attributes['start_date(3i)'].to_i)
-    @end_date = Date.new(attributes['end_date(1i)'].to_i, attributes['end_date(2i)'].to_i, attributes['end_date(3i)'].to_i)
-    @type = attributes['type']
+    new_hash = Hash.new
+    DATE_ATTRIBUTE_MAPPING.each do |mapping|
+      new_key = mapping[0]
+      initial_key = mapping[1]
+      new_hash[new_key] = attributes[initial_key] ? attributes[initial_key].to_i : nil
+    end
+
+    super new_hash
   end
 
   def csv_string
@@ -20,9 +26,21 @@ class ActivityReport
     "#{type} #{formatted_start_date}-#{formatted_end_date}.csv"
   end
 
+  def has_data?
+    activities.present?
+  end
+
   private
 
+  def start_date
+    @start_date ||= Date.new(start_year, start_month, start_day)
+  end
+
+  def end_date
+    @end_date ||= Date.new(end_year, end_month, end_day)
+  end
+
   def activities
-    Activity.includes(:location, :judge).between_dates(start_date, end_date)
+    @activities ||= Activity.includes(:location, :judge).between_dates(start_date, end_date)
   end
 end
