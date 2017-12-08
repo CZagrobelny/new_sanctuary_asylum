ec21a2e334971ad2fa19cf3743c80aa689b8b78d
Thomas Countz <thomascountz@gmail.com>
2017-12-08 10:46:00 -0500

    green lit spec for csv_string method

--- app/models/activity_report.rb
+++ app/models/activity_report.rb
@@ -1,8 +1,24 @@
+# frozen_string_literal: true
 class ActivityReport < Report
 
   def csv_string
-    # This is replacing the generate method
+    column_headers = ['Event', 'Date', 'Location Name', 'Judge', 'No. of Accompaniments']
+
+    CSV.generate(headers: true) do |csv|
+      csv << column_headers
+
+      data.each do |activity|
+        row = []
+        row << activity.event
+        row << activity.occur_at.to_s
+        row << activity.location.name
+        row << activity.judge.name
+        row << activity.accompaniments.count.to_s
+        csv << row
+      end
+    end
   end
+ 
 
   private
 
