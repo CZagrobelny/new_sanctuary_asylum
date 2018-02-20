e96fbba0f6a9635c5e5625d1bb1f2d0ae5c8dde6
Mike <zluo@alumni.berklee.edu>
2018-02-20 16:11:30 -0500

    Added uniqueness validation for user id scoped to events in user events model and specs

--- app/models/user_event_attendance.rb
+++ app/models/user_event_attendance.rb
@@ -3,4 +3,5 @@ class UserEventAttendance < ActiveRecord::Base
   belongs_to :event
   validates :user_id, :event_id, presence: true
   validates :user_id, numericality: { greater_than: 0 }
+  validates :user_id, uniqueness: { scope: :event_id }
 end
