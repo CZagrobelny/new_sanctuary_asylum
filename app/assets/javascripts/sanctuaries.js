e13556412dfbb02199eef9759992de1ee35a53fd
RSL <sconds@gmail.com>
2018-02-21 12:06:06 -0500

    use simpler code for chosen selects

--- app/assets/javascripts/sanctuaries.js
+++ app/assets/javascripts/sanctuaries.js
@@ -3,13 +3,9 @@ $(document).on('turbolinks:load', function () {
 });
 
 function activateSanctuariesChosen() {
-  var all_chosen_selects = $('.chosen-select');
-  for (var i=0; i<all_chosen_selects.length;i++) {
-    var chosen_select = all_chosen_selects[i];
-    $(chosen_select).chosen({
-      allow_single_deselect: true,
-      no_results_text: 'No results matched',
-      width: '100%'
-    });
-  }
+  $('.chosen-select').chosen({
+    allow_single_deselect: true,
+    no_results_text: 'No results matched',
+    width: '100%'
+  });
 }
