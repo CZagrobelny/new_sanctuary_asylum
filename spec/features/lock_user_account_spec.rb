3cdee2aa90e1fc26540defb0c268d4c7a015fb63
Jimmy Adames <jimmy.adames@gmail.com>
2018-02-23 10:36:36 -0500

    Adjusted feature specs for a passing test suite

--- spec/features/lock_user_account_spec.rb
+++ spec/features/lock_user_account_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe 'User account is locked', type: :feature do
+RSpec.describe 'User account gets locked', type: :feature do
 
   let(:user) { create(:user) }
 
-  scenario 'User has one attempt left before their account is locked' do
+  scenario 'logging in with invalid credentials after eight attempts' do
+    visit root_path
+
+    8.times do
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: 'password'
+      click_on 'Log in'
+    end
+
+    user.reload
+    expect(user.failed_attempts).to eq 8
+
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: 'password'
+    click_on 'Log in'
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'You have 1 more attempt before your account is locked.'
+  end
+
+  scenario 'logging in with invalid credentials after nine attempts' do
     visit root_path
 
     9.times do
@@ -19,26 +38,7 @@ RSpec.describe 'User account is locked', type: :feature do
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: 'password'
     click_on 'Log in'
-    expect(current_path).to eq root_path
-    expect(page).to have_content 'You have one more attempt before your account is locked.'
-  end
-
-  scenario 'User attempts to log in after 10 failed attempts' do
-    visit root_path
-
-    10.times do
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: 'password'
-      click_on 'Log in'
-    end
-
-    user.reload
-    expect(user.failed_attempts).to eq 10
-
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: 'password'
-    click_on 'Log in'
-    expect(current_path).to eq root_path
+    expect(current_path).to eq new_user_unlock_path
     expect(page).to have_content 'Your account is locked.'
   end
 end
