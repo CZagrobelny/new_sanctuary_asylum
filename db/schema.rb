b77c8d9897d71b2fddf62b70050a199cc7598721
szeidman <sam.zeidman@gmail.com>
2018-02-26 16:46:58 -0500

    Add order scope for accompaniments in Activity model and add timestamps to accompaniments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222204140) do
+ActiveRecord::Schema.define(version: 20180226213127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,11 @@ ActiveRecord::Schema.define(version: 20180222204140) do
   end
 
   create_table "accompaniments", force: :cascade do |t|
-    t.integer "activity_id"
-    t.integer "user_id"
-    t.text    "availability_notes"
+    t.integer  "activity_id"
+    t.integer  "user_id"
+    t.text     "availability_notes"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "activities", force: :cascade do |t|
