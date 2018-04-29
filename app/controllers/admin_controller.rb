110f377623c8f79f278e9b1af50575ade7ec1fe5
CZagrobelny <zagrobel@gmail.com>
2018-04-29 12:14:26 -0400

    log admin actions and queries

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,22 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
   before_action :require_admin
+  after_action :log_action
 
   private
   def require_admin
     not_found unless current_user.admin?
+  end
+
+  def log_action
+    Rails.logger.info "ADMIN [#{current_user.email}, ip: #{request.remote_ip}]:  #{request_path_parameters} #{request_query_parameters}"
+  end
+
+  def request_path_parameters
+    "PATH: #{request.path_parameters}"
+  end
+
+  def request_query_parameters
+    "QUERY: #{request.query_parameters}" if request.query_parameters.present?
   end
 end
