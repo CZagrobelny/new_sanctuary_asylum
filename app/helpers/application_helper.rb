65fd445cfbee1cb7b588fd43b48f8483205caf5b
CZagrobelny <zagrobel@gmail.com>
2018-04-29 14:58:03 -0400

    add timeout for inactive user and prevent blank timeout flash message from displaying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,7 @@ module ApplicationHelper
   end
 
   def flash_messages(opts = {})
-    flash.each do |msg_type, message|
+    flash.delete('timedout').each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
         concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
         concat message
