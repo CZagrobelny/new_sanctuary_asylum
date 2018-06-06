43b977d5ace237843506a16f2648d4acacafda50
CZagrobelny <zagrobel@gmail.com>
2018-06-06 09:46:26 -0400

    begin adding access controller specs and fix issue with region factory

--- spec/factories/region_factory.rb
+++ spec/factories/region_factory.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :region do
-    name { FFaker::Address.us_state.downcase.gsub(/[^a-z]/i, '') }
+    name { FFaker::Company.name.downcase.gsub(/[^a-z]/i, '') }
   end
 end
