1a96e2466f689222724c20893d4cf64377c83548
Robbie Holmes <robbiethegeek@gmail.com>
2018-06-09 19:00:21 -0400

    Add checkbox to invite user form for regional admins.
    Co-authored-by: Asha <agolveo@gmail.com>
    Co-authored-by: Robbie Holmes <robbiethegeek@gmail.com>

--- spec/features/invite_a_new_user_spec.rb
+++ spec/features/invite_a_new_user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Admin invites a new user', type: :feature do
 
   let(:admin) { create(:user, :community_admin, community: community) }
+  let(:regional_admin) { create(:user, :regional_admin, community: community) }
   let(:community) { create(:community) }
   let(:email) { FFaker::Internet.email }
 
@@ -20,11 +21,24 @@ RSpec.describe 'Admin invites a new user', type: :feature do
       end
     end
 
+    it 'does not display the remote clinic lawyer checkbox' do
+      expect(page).to_not have_content('Remote Clinic Lawyer')
+    end
+
     it 'sends the invitation' do
       expect{ click_button 'Send an invitation' }.to change(ActionMailer::Base.deliveries, :count).by(1)
     end
   end
-
+  describe 'remote admin inviting a new user' do
+    before do
+      login_as(regional_admin)
+      visit new_user_community_invitation_path(community)
+      fill_in 'Email', with: email
+    end
+    it 'does display the remote clinic lawyer checkbox' do
+      expect(page).to have_content('Remote Clinic Lawyer')
+    end
+  end
   describe 'user accepting an invitation' do
     before do
       login_as(admin)
