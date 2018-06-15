d5f9f58ffc58ec2956e6bc27dcdef78899f8e5ad
freen <dbickett@gmail.com>
2018-06-15 19:35:30 -0400

    friend 1 yr deadline: filters load open if deadline fields populated

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,9 +1,17 @@
 module FriendsHelper
+  DATE_REGEXP = /\d{4}\-\d{2}-\d{2}/
+
   def friend_search_filtered?
     {
-      detained: 'yes'
+      detained: 'yes',
+      deadline_start_date: DATE_REGEXP,
+      deadline_start_date: DATE_REGEXP
     }.any? do |key, value|
-      params[key] == value
+      if value.is_a?(Regexp)
+        value.match(params[key])
+      else
+        params[key] == value
+      end
     end
   end
 end
