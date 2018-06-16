252bf1f6909e4efa09ae6f02c2f86bf8ab0af824
freen <dbickett@gmail.com>
2018-06-16 08:27:51 -0400

    friend 1 yr deadline: name constant for source of dateformat, better clarity

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,11 +1,11 @@
 module FriendsHelper
-  DATE_REGEXP = /\d{4}\-\d{2}-\d{2}/
+  DATEPICKER_DATE_REGEXP = /\d{4}\-\d{2}-\d{2}/
 
   def friend_search_filtered?
     {
       detained: 'yes',
-      deadline_start_date: DATE_REGEXP,
-      deadline_end_date: DATE_REGEXP
+      deadline_start_date: DATEPICKER_DATE_REGEXP,
+      deadline_end_date: DATEPICKER_DATE_REGEXP
     }.any? do |key, value|
       if value.is_a?(Regexp)
         value.match(params[key])
