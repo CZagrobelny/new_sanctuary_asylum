7e9eda97f40a60f396f6fe5e8897310a9c6e75c5
freen <dbickett@gmail.com>
2018-06-23 21:06:33 -0400

    fix SMTP exceptions, absent From field in env=test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_options = { from: 'test@test.com' }
 
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
