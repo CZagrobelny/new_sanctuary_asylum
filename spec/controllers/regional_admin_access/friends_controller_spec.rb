f273e0a0ae63974d934181c1493bcbcfa5545308
CZagrobelny <zagrobel@gmail.com>
2018-06-24 12:14:43 -0400

    expand regional_admin_access communities and friends controller specs to verify that non-regional admins hit 404s

--- spec/controllers/regional_admin_access/friends_controller_spec.rb
+++ spec/controllers/regional_admin_access/friends_controller_spec.rb
@@ -2,27 +2,51 @@ require 'rails_helper'
 
 RSpec.describe RegionalAdmin::FriendsController, type: :controller do
 
-  let(:regional_admin) { create :user, :regional_admin, community: community }
   let(:community) { create :community, region: region }
   let(:region) { create :region }
   let(:friend) { create :friend, community: community, region: region }
 
-  before do
-    allow(controller).to receive(:authenticate_user!).and_return(true)
-    allow(controller).to receive(:current_user).and_return(regional_admin)
-  end
+  describe 'as a logged in regional admin' do
+    let(:regional_admin) { create :user, :regional_admin, community: community }
 
-  describe 'GET #index' do
-    it 'allows access' do
-      get :index, params: { region_id: region.id }
-      expect(response.success?).to eq true
+    before do
+      allow(controller).to receive(:authenticate_user!).and_return(true)
+      allow(controller).to receive(:current_user).and_return(regional_admin)
+    end
+
+    describe 'GET #index' do
+      it 'allows access' do
+        get :index, params: { region_id: region.id }
+        expect(response.success?).to eq true
+      end
+    end
+
+    describe 'GET #new' do
+      it 'allows access' do
+        get :show, params: { region_id: region.id, id: friend.id }
+        expect(response.success?).to eq true
+      end
     end
   end
 
-  describe 'GET #new' do
-    it 'allows access' do
-      get :show, params: { region_id: region.id, id: friend.id }
-      expect(response.success?).to eq true
+  describe 'as a logged in community admin' do
+    let(:community_admin) { create :user, :community_admin, community: community }
+
+    before do
+      allow(controller).to receive(:authenticate_user!).and_return(true)
+      allow(controller).to receive(:current_user).and_return(community_admin)
+    end
+
+    describe 'GET #index' do
+      it 'does NOT allow access' do
+        expect{ get :index, params: { region_id: region.id } }.to raise_error('Not Found')
+      end
+    end
+
+    describe 'GET #new' do
+      it 'does NOT allow access' do
+        expect{ get :show, params: { region_id: region.id, id: friend.id } }.to raise_error('Not Found')
+      end
     end
   end
 end
