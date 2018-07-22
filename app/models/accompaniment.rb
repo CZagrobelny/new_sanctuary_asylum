68aac807cf0e8fdd09d0b4182545c0d2c1badf60
Natasha Jones <tasha.k.jones@gmail.com>
2018-07-22 19:14:38 -0400

    Add .limit_for_family_court to Accompaniment model

--- app/models/accompaniment.rb
+++ app/models/accompaniment.rb
@@ -3,6 +3,7 @@ class Accompaniment < ApplicationRecord
   belongs_to :activity
 
   validates :activity_id, :user_id, presence: true
+  validate :limit_for_family_court
 
   def self.find_or_build(activity, user)
     if user.attending?(activity)
@@ -11,4 +12,24 @@ class Accompaniment < ApplicationRecord
       user.accompaniments.new(activity_id: activity.id)
     end
   end
+
+  private
+
+  def limit_for_family_court
+    if limit_reached? 
+      errors.add(:activity, family_court_error)
+      false
+    end
+  end
+
+  def limit_reached?
+    return false if activity.try(:event) != 'family_court'
+
+    activity.accompaniments.count >= 3
+  end
+
+  def family_court_error
+    "Only 3 accompaniments are allowed per family_court activity"
+  end
+
 end
