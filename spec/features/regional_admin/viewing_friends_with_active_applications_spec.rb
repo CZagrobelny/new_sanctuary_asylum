6d50a49f28b011cfa9f06b4c59e8f05331dc0ef0
Brooke <brooke@noredink.com>
2018-08-03 14:41:15 -0400

    add spec for when user has other applications

--- spec/features/regional_admin/viewing_friends_with_active_applications_spec.rb
+++ spec/features/regional_admin/viewing_friends_with_active_applications_spec.rb
@@ -23,24 +23,46 @@ RSpec.describe 'Regional Admin views friends with active applications', type: :f
       end
     end
 
-    it 'closes an application and destroys the remote user_friend_associations' do
-      visit regional_admin_region_friend_path(region, friend)
+    describe "without other applications" do
+      it 'closes an application and destroys the remote user_friend_associations' do
+        visit regional_admin_region_friend_path(region, friend)
 
-      category = application.category.titlecase
-      click_on("Close #{category} Application")
+        category = application.category.titlecase
+        click_on("Close #{category} Application")
 
-      within '#friends-with-active-applications' do
-        expect(page).to_not have_link(friend.name)
+        within '#friends-with-active-applications' do
+          expect(page).to_not have_link(friend.name)
+        end
+
+        expect(page).to have_content('Friends with Active Applications')
+
+        success_text = 'Application closed and assigned remote clinic lawyers removed. Friend has been removed from Active Applications dashboard.'
+        expect(page).to have_content(success_text)
+
+        expect(friend.reload.users).to be_empty
+
+        expect(application.reload.status).to eq('closed')
       end
+    end
 
-      expect(page).to have_content('Friends with Active Applications')
+    describe "with other applications" do
+      let!(:other_application) { create :application, friend: friend, status: 'in_review', category: "sijs" }
 
-      success_text = 'Application closed and assigned remote clinic lawyers removed. Friend has been removed from Active Applications dashboard.'
-      expect(page).to have_content(success_text)
+      it 'closes an application and does not destroy the remote user_friend_associations' do
+        visit regional_admin_region_friend_path(region, friend)
 
-      expect(friend.reload.users).to be_empty
+        category = application.category.titlecase
+        click_on("Close #{category} Application")
 
-      expect(application.reload.status).to eq('closed')
+        expect(page).to have_link(friend.name)
+
+        success_text = 'Application closed.'
+        expect(page).to have_content(success_text)
+
+        expect(friend.reload.users).to match_array([remote_clinic_lawyer])
+
+        expect(application.reload.status).to eq('closed')
+      end
     end
   end
 
