85f58f7b12183f990e0d0fe05b3c5f909a5fbf0d
RSL <sconds@gmail.com>
2018-11-30 11:15:29 -0500

    customize generators to no longer autogenerate some files
    no automatic tests, stylesheets, js, helpers. we can add them as needed with intention.

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,12 @@ module NewSanctuaryAsylum
     if !Rails.env.development? && !Rails.env.test?
       config.middleware.use Rack::Attack
     end
+
+    config.generators do |g|
+      g.test_framework false
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
