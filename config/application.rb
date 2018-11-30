a2c8a272df80915afef91d5c699ebfaaa750880c
RSL <sconds@gmail.com>
2018-11-30 11:13:58 -0500

    fix indentation

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,10 @@ module NewSanctuaryAsylum
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-		#Allow the app to controll it's own error routes (custom 404, etc.)
-		#NOTE:and app erros will still cause the server env to handle errors (Heroku), so these routes are only effective when the app is running
-	config.exceptions_app = self.routes
+    # Allow the app to controll it's own error routes (custom 404, etc.)
+    # NOTE:and app errors will still cause the server env to handle errors (Heroku),
+    # so these routes are only effective when the app is running
+  	config.exceptions_app = self.routes
     if !Rails.env.development? && !Rails.env.test?
       config.middleware.use Rack::Attack
     end
