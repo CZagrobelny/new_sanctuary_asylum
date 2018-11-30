e7d361080cfe0195e1846c5e710ffc2c558474bc
RSL <sconds@gmail.com>
2018-11-30 13:30:11 -0500

    add validations for release

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,5 +1,12 @@
 class Release < ApplicationRecord
+  TYPES = %w{
+    limited_scope
+  }.freeze
+
   belongs_to :friend
 
   mount_uploader :release_form, ReleaseFormUploader
+
+  validates :release_form, presence: true
+  validates :category, presence: true, inclusion: { in: TYPES }
 end
