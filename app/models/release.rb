dabe55d5fe14822c0d187be8a56bf54ac0c8a625
RSL <sconds@gmail.com>
2018-12-05 12:33:01 -0500

    cleanup validations on releases

--- app/models/release.rb
+++ app/models/release.rb
@@ -8,6 +8,14 @@ class Release < ApplicationRecord
 
   mount_uploader :release_form, ReleaseFormUploader
 
-  validates :release_form, presence: true
-  validates :category, presence: true, inclusion: { in: TYPES }
+  validates :release_form, :friend, presence: true
+  validates :category, presence: true, inclusion: { in: TYPES }, uniqueness: { scope: [:friend_id, :user_id] }
+  validate :user_is_lawyer
+
+  private
+
+  def user_is_lawyer
+    return unless user
+    errors.add(:user, 'must be lawyer') unless user.volunteer_type == 'lawyer'
+  end
 end
