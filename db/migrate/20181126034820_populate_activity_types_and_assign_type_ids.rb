222dd42ba32bf13e79a2e14c5a3cd80ab9fa2880
yeti-detective <yeti.detective@gmail.com>
2018-12-05 22:50:11 -0500

    migration to populate activity_types from Activity model

--- db/migrate/20181126034820_populate_activity_types_and_assign_type_ids.rb
+++ db/migrate/20181126034820_populate_activity_types_and_assign_type_ids.rb
@@ -1,4 +1,21 @@
 class PopulateActivityTypesAndAssignTypeIds < ActiveRecord::Migration[5.0]
   def change
+    eligible_events = Activity::ACCOMPANIMENT_ELIGIBLE_EVENTS
+    ineligible_events = Activity::NON_ACCOMPANIMENT_ELIGIBLE_EVENTS
+
+    for event in eligible_events do
+      new_activity_type = ActivityType.new(name: event, accompaniment_eligible: true)
+      new_activity_type.save
+    end
+    for event in ineligible_events do
+      new_activity_type = ActivityType.new(name: event, accompaniment_eligible: false)
+      new_activity_type.save
+    end
+
+    all_activities = Activity.all
+    all_activities.each do |activity|
+      activity.activity_type = ActivityType.find_by(name: activity.event)
+      activity.save
+    end
   end
 end
