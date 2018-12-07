bf857003a117ad40867a73d74f8aeca39950cbce
yeti-detective <yeti.detective@gmail.com>
2018-12-07 17:20:53 -0500

    migration to set accompaniment limit for family_court activity type

--- db/migrate/20181206141243_family_court_cap.rb
+++ db/migrate/20181206141243_family_court_cap.rb
@@ -2,7 +2,7 @@ class FamilyCourtCap < ActiveRecord::Migration[5.0]
   def change
     fc_act_type = ActivityType.find_by(name: 'family_court')
     if fc_act_type
-      fc_act_type.cap = 2
+      fc_act_type.cap = 3
       fc_act_type.save
     end
   end
