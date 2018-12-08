b2336e46314c08758bc20dfa00ecfec08b234fea
yeti-detective <yeti.detective@gmail.com>
2018-12-08 12:34:08 -0500

    master_calendar_hearing trait added

--- spec/factories/activity_type_factory.rb
+++ spec/factories/activity_type_factory.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     name 'family_court'
     cap 3
   end
+
+  trait :master_calendar_hearing do
+    name 'master_calendar_hearing'
+    id name: "master_calendar_hearing"
+  end
 end
