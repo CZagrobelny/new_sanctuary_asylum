2ca555157a5f494d192536d7f45078dbef90c5f8
CZagrobelny <zagrobel@gmail.com>
2019-01-30 21:00:22 -0500

    prevent next month accompaniment flash notification from overriding rsvp flash notifications

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,8 +6,10 @@ class ActivitiesController < ApplicationController
   def index
     week1, week2 = DatesHelper.two_weeks
 
-    if listed_activities_are_at_end_of_month(week1, week2) and next_month_has_posted_activities(week2)
-      flash[:success] = "Accompaniments posted for next month! Click 'Next' in the calendar to view future accompaniments."
+    if activities_listed_next_month?(week1, week2)
+      unless flash[:success]
+        flash[:success] = "Accompaniments posted for next month! Click 'Next' in the calendar to view future accompaniments."
+      end
     end
     @upcoming_activities = current_region.activities
                                          .for_time_confirmed(week1.begin, week2.end)
@@ -16,11 +18,8 @@ class ActivitiesController < ApplicationController
 
   private
 
-  def listed_activities_are_at_end_of_month(week1, week2)
-    week1.include?(week1.begin.end_of_month) || week2.include?(week2.begin.end_of_month)
-  end
-
-  def next_month_has_posted_activities(week2)
+  def activities_listed_next_month?(week1, week2)
+    week1.begin.month != week2.end.month &&
     current_region.activities.for_time_confirmed(week2.begin.beginning_of_month, week2.end).any?
   end
 end
