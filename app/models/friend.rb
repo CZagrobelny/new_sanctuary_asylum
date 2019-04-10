356829eae49dd96201e757034d22ed4360625dd8
CZagrobelny <zagrobel@gmail.com>
2019-04-10 08:16:16 -0400

    filter out null values when sorting by asc/desc on friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -135,13 +135,13 @@ class Friend < ApplicationRecord
       # Simple sort on the created_at column.
       order("friends.created_at #{direction}")
     when /^border_queue_number/
-      order("friends.border_queue_number #{direction}")
+      where('border_queue_number IS NOT NULL').order("friends.border_queue_number #{direction}")
     when /^intake_date_/
-      order("friends.intake_date #{direction}")
+      where('intake_date IS NOT NULL').order("friends.intake_date #{direction}")
     when /^must_be_seen_by_/
-      order("friends.must_be_seen_by #{direction}")
+      where('must_be_seen_by IS NOT NULL').order("friends.must_be_seen_by #{direction}")
     when /^date_of_entry/
-      order("friends.date_of_entry #{direction}")
+      where('date_of_entry IS NOT NULL').order("friends.date_of_entry #{direction}")
     else
       raise(ArgumentError, "Invalid sort option: #{sort_option.inspect}")
     end
