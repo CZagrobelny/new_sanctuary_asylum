f4387cccd17de86170486f9fd132be6e6ab9fe24
Emily Gasca <oh.emi1337@gmail.com>
2019-04-09 21:41:28 -0400

    [Friends] Add Intake Dates Fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_004748) do
+ActiveRecord::Schema.define(version: 2019_04_10_013846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -206,6 +206,8 @@ ActiveRecord::Schema.define(version: 2019_04_10_004748) do
     t.string "city"
     t.string "jail_id"
     t.text "intake_notes"
+    t.datetime "intake_date"
+    t.datetime "must_be_seen_by"
     t.index ["community_id"], name: "index_friends_on_community_id"
     t.index ["region_id"], name: "index_friends_on_region_id"
   end
