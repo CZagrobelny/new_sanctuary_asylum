4c7b851934fe6cc927757c6eac63786207b4fda6
freen <dbickett@gmail.com>
2019-09-27 16:23:06 -0400

    remove comment

--- app/models/accompaniment.rb
+++ app/models/accompaniment.rb
@@ -1,24 +1,3 @@
-# Options
-# 1. Make `belongs_to :activity` polymorphic, possibly mapping to Carpool. Requires renaming.
-#    Trouble here is naming. `activity_type` is already spoken for, m/w there's not much more
-#    generic than `activity`. `event` is also spoken for. S/t like `accompaniment_need` could work, but
-#    this language is awkward / indirect.
-#      No: activity, event, accompaniment_need,
-#
-# 2. Create second `belongs_to :carpool` and both are optional. Disadvantages include: `if` conditional
-#    everythwere .activity is now used (nowhere..)
-
-# Choice to have two optional belongs_to is part semantic and part rational.
-
-# Semantic: Naming things is important. In the application vocab, event and activity are both taken. There is little
-# which is more generic than that.
-
-# Rational: Activity is already as generic as it gets. Activities have types. Accompanying for an accompaniment-
-# eligible activity already covers the gamut of possible accompaniments. In other words, creating a polymorphic
-# association from the accopmaniments table to "anything" is redundant and unneeded complexity: activities already
-# are "anything." We only need one more possible association, which is the carpool (a grouping of N activities of X
-# types.) Done!
-
 class Accompaniment < ApplicationRecord
   belongs_to :user
   belongs_to :activity
