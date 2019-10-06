5ca790607c4efda3304154ff687b8a08fbd9767e
Sabrina Gannon <sabrinagannon@icloud.com>
2019-10-06 14:17:45 -0400

    Database changes: Add table for ankle monitors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_05_142654) do
+ActiveRecord::Schema.define(version: 2019_10_06_163114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 2019_10_05_142654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "accompaniment_eligible"
+  end
+
+  create_table "ankle_monitors", force: :cascade do |t|
+    t.datetime "date_removed"
+    t.boolean "bi_smart_link"
+    t.text "notes"
+    t.bigint "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_ankle_monitors_on_friend_id"
   end
 
   create_table "applications", id: :serial, force: :cascade do |t|
