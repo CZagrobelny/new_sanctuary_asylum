cfa9a3c1deca7930aa030ce7801545695a0d819f
Omri Bernstein <omri.bernstein@newsela.com>
2019-11-15 15:05:54 -0500

    Rename migration association to singular for categories and include controller fields for the referral ids and notes

--- app/models/friend_social_work_referral_category.rb
+++ app/models/friend_social_work_referral_category.rb
@@ -1,4 +1,4 @@
 class FriendSocialWorkReferralCategory < ApplicationRecord
     belongs_to :friend
-    belongs_to :friend_social_work_referral_category
+    belongs_to :social_work_referral_category
 end
--- db/migrate/20191115163908_create_friend_social_work_referral_categories.rb
+++ db/migrate/20191115163908_create_friend_social_work_referral_categories.rb
@@ -2,7 +2,7 @@ class CreateFriendSocialWorkReferralCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :friend_social_work_referral_categories do |t|
       t.belongs_to :friend, :null => false
-      t.belongs_to :social_work_referral_categories, index: { name: :index_fswrc_on_swrc_id }, :null => false
+      t.belongs_to :social_work_referral_category, index: { name: :index_fswrc_on_swrc_id }, :null => false
 
       t.timestamps :null => false
     end
