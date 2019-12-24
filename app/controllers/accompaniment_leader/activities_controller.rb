7ee753ce0000a2cca156e30787de8524928d6c6d
CZagrobelny <zagrobel@gmail.com>
2019-12-24 12:33:56 -0500

    fix spacing

--- app/controllers/accompaniment_leader/activities_controller.rb
+++ app/controllers/accompaniment_leader/activities_controller.rb
@@ -2,7 +2,7 @@ class AccompanimentLeader::ActivitiesController < AccompanimentLeaderController
   def index
     start_date, end_date = DatesHelper.five_weeks
     @upcoming_activities = current_region.activities
-                                         .for_time_confirmed(start_date, end_date)
-                                         .includes(:accompaniments, :users, :accompaniment_reports, :friend, :location)
+      .for_time_confirmed(start_date, end_date)
+      .includes(:accompaniments, :users, :accompaniment_reports, :friend, :location)
   end
 end
