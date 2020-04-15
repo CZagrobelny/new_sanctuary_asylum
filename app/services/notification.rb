bf352ffaca45127b7c05bff7d1068637d074dbaf
boom100100 <bsd231@nyu.edu>
2020-04-15 10:04:55 -0400

    Deleted services/notification.

--- app/services/notification.rb
+++
@@ -1,9 +0,0 @@
-class Notification
-  def self.draft_for_review(draft: nil)
-    if draft.in_review? && draft.friend.remote_clinic_lawyers.present?
-      ReviewMailer.review_needed_email(draft).deliver_now
-    elsif draft.in_review?
-      ReviewMailer.lawyer_assignment_needed_email(draft).deliver_now
-    end
-  end
-end
