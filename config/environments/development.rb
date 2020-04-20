a5d5be70a9ee955a8e8e73b3ebae478ffadd50ef
RSL <sconds@gmail.com>
2020-04-20 19:58:29 -0400

    improve ux for regional admins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.log_level = :warn
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
