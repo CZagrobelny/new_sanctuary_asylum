fc8107bf46150444f5286d7c91bf29ca61c2a816
CZagrobelny <zagrobel@gmail.com>
2020-08-21 20:59:58 -0400

    initiate select2 with bootstrap theme on all pages except search

--- app/assets/javascripts/sanctuaries.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function () {
-  activateSanctuariesSelect2();
-});
-
-function activateSanctuariesSelect2() {
-  $('.js-select2-sanctuaries').select2({
-    width: '100%'
-  });
-}
