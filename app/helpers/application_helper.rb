fab8dbb2b22ef73e2f327961fabae0dcee71159f
RSL <sconds@gmail.com>
2020-09-16 19:40:36 -0400

    simplify flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,16 +20,6 @@ module ApplicationHelper
     BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type.to_s)
   end
 
-  def flash_messages(_opts = {})
-    flash.delete('timedout').each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: 'alert') do
-        concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
-        concat message
-      end)
-    end
-    nil
-  end
-
   def locations_by_name(region)
     region.locations.order('name')
   end
