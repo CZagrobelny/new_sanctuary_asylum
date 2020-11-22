6eaad5cdbbd823c6492d024f74bb965ae988d4b7
CZagrobelny <zagrobel@gmail.com>
2020-11-22 11:40:04 -0500

    small tweaks

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,8 +1,8 @@
 class Application < ApplicationRecord
   CATEGORIES = %w[appeal asylum asylum_supplementary_docs change_of_venue
-                  foia detention habeas_corpus government_issued_document
-                  humanitarian_parole identifying_document intake
-                  motion_to_reopen nsc_paperwork other remote_review_*attorney_only*
+                  detention foia government_issued_document habeas_corpus
+                  humanitarian_parole identifying_document individual_hearing_material
+                  intake motion_to_reopen nsc_paperwork other remote_review_*attorney_only*
                   report_back sijs work_authorization team_sheet].freeze
 
   has_many :drafts, dependent: :restrict_with_error
