55a91a29a2397ae8e312d6fc481092699ec21daa
CZagrobelny <zagrobel@gmail.com>
2020-12-22 18:52:52 -0500

    allow eoir callers to confirm/unconfirm activities

--- app/controllers/eoir_caller/activities_controller.rb
+++ app/controllers/eoir_caller/activities_controller.rb
@@ -6,4 +6,35 @@ class EoirCaller::ActivitiesController < EoirCallerController
       .where('occur_at < ?', 3.weeks.from_now.end_of_week)
       .includes(:users, :friend, :location)
   end
+
+  def confirm
+    if activity.update(confirmed: true)
+      flash[:success] = 'Accompaniment confirmed.'
+    else
+      flash.now[:error] = 'There was an issue confirming this accompaniment.'
+    end
+    redirect_to community_eoir_caller_activities_path(current_community, start_date: start_date)
+  end
+
+  def unconfirm
+    if activity.update(confirmed: false)
+      flash[:success] = 'Accompaniment confirmation removed.'
+    else
+      flash.now[:error] = 'There was an issue unconfirming this accompaniment.'
+    end
+    redirect_to community_eoir_caller_activities_path(current_community, start_date: start_date)
+  end
+
+  private
+  def activity
+    @activity ||= current_region.activities.find(params[:id])
+  end
+
+  def start_date
+    @start_date ||= if activity.occur_at?
+      activity.occur_at.beginning_of_week.to_date
+    else
+      Time.now.beginning_of_week.to_date
+    end
+  end
 end
